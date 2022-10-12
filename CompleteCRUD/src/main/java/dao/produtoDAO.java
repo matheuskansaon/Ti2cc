c96ce70bf71cbd47de5c8687fdbd653e050d74c7
Natannts <45772080+Natannts@users.noreply.github.com>
2022-10-12 17:49:20 -0300

    Create produtoDAO.java

---
+++ CompleteCRUD/src/main/java/dao/produtoDAO.java
@@ -0,0 +1,84 @@
+package dao;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import model.Produto;
+
+public class ProdutoDAO extends DAO{
+	public ProdutoDAO() {
+		conectar();
+	}
+
+	public void finalize() {
+		close();
+	}
+	
+	public boolean insert(Produto produto) {
+		boolean status = false;
+		try {
+			String sql = "INSERT INTO Produto (id, preco, ProdutoID, RestauranteID, "
+					   + "logo"
+		               + "VALUES ('" + restaurante.getID() + "', " + restaurante.getPreco() + ", "
+		               + restaurante.getProdutoID() + ", " + restaurante.getRestauranteID() + ", "
+		               + restaurante.getUrlLogo();
+			PreparedStatement st = conexao.prepareStatement(sql);
+		    //st.setTimestamp(1, Timestamp.valueOf(restaurante.getDataFabricacao()));
+			//st.setDate(2, Date.valueOf(restaurante.getDataValidade()));
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {  
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+	
+	public Restaurante get(int id) {
+		Restaurante restaurante = null;
+		
+		try {
+			Statement st = (Statement) conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM produto WHERE id="+id;
+			ResultSet rs = ((java.sql.Statement) st).executeQuery(sql);	
+	        if(rs.next()){            
+	        	 produto = new Produto(rs.getInt("id"), rs.getDouble("preco"), 
+	        			 			   rs.getInt("ProdutoID"), 
+	                				   rs.getInt("RestauranteID"),
+	                				   rs.getString("logo"));
+	        }
+	        ((java.sql.Statement) st).close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return restaurante;
+	}
+	
+
+	// RECUPERA LISTA DE RESTAURANTES
+	public List<Produto> get() {
+		List<Produto> restaurantes = new ArrayList<Produto>();
+		
+		try {
+			Statement st = conexao.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			String sql = "SELECT * FROM restaurant" ;
+			ResultSet rs = st.executeQuery(sql);	           
+	        while(rs.next()) {	            	
+	        	produto r = new Produto(rs.getInt("id"), rs.getDouble("preco"), 
+	        			 			   rs.getInt("ProdutoID"), 
+	                				   rs.getInt("RestauranteID"),
+	                				   rs.getString("logo"))
+	        			                ;
+	            produto.add(r);
+	        }
+	        st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return produto;
+	}
+}
