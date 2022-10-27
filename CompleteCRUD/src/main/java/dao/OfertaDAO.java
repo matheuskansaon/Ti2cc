e38ba84264666603285b0cac2c3298b6e5cd585b
Camilla Teodoro <camillateodoro@hotmail.com>
2022-10-27 16:30:03 -0300

    removido código js antigo

--- CompleteCRUD/src/main/java/dao/OfertaDAO.java
+++ CompleteCRUD/src/main/java/dao/OfertaDAO.java
@@ -3,8 +3,7 @@ import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 import model.Oferta;
